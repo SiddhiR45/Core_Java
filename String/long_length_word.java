b15381d24035b78daa0a5ec90cb7b36ca98dc0bc
SiddhiR45 <siddhigawali16@gmail.com>
2024-10-15 13:23:58 +0530

    Add files via upload

---
+++ String/long_length_word.java
@@ -0,0 +1,76 @@
+package String;
+
+public class long_length_word {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println(longlength("Java is a programming lanuage"));
+	duplichar("programming java lanuage");
+	duplichar1("java is programming lanuage");
+	}
+	
+	public static String longlength(String s)
+	{
+		String a[]=s.split(" ");
+		String ans=" ";
+		int maxilength=Integer.MIN_VALUE;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i].length()>maxilength)
+			{
+				maxilength=a[i].length();
+				ans=a[i];
+			}
+		}
+		return ans;
+		
+	}
+	public static void duplichar(String s)
+	{
+		System.out.println("duplicate char");
+		//String ans="";
+		int a[]=new int[256];
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)!=' ') {
+			a[s.charAt(i)]++;
+			}
+			
+		}
+	
+		for(int i=0;i<a.length;i++)
+		{	
+			if(a[i]>1)
+			{
+				System.out.print((char)i+",");
+			}
+		}
+		System.out.println();
+		
+	}
+	
+	public static void duplichar1(String s)
+	{
+		System.out.println("uninque char");
+		//String ans="";
+		int a[]=new int[256];
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)!=' ') {
+			a[s.charAt(i)]++;
+			}
+			
+		}
+	
+		for(int i=0;i<a.length;i++)
+		{	
+			if(a[i]==1)
+			{
+				System.out.print((char)i+",");
+			}
+		}
+		
+	}
+
+
+}
